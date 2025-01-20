765f05480225cdf2ce6d9832c56c366a2fd65b42
Bugo <cheetah@bk.ru>
2025-01-20 16:32:00 +0500

    Upload initial files

---
+++ astro.config.mjs
@@ -0,0 +1,55 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightLinksValidator from 'starlight-links-validator';
+import svelte from '@astrojs/svelte';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [starlight({
+    plugins: [
+      starlightLinksValidator({
+        errorOnRelativeLinks: false,
+      }),
+    ],
+    title: 'Svelte по-русски',
+    description: 'Документация Svelte 5 на русском языке.',
+    defaultLocale: 'root',
+    locales: {
+      root: {
+        label: 'Русский',
+        lang: 'ru',
+      },
+    },
+    logo: {
+      dark: './src/assets/dark_logo.svg',
+      light: './src/assets/logo.svg',
+      replacesTitle: true,
+      alt: 'Логотип Svelte',
+    },
+    social: {
+      github: 'https://github.com/dragomano/svelte-russian',
+    },
+    editLink: {
+      baseUrl: 'https://github.com/dragomano/svelte-russian/edit/main/',
+    },
+    sidebar: [
+      {
+        label: 'Введение',
+        autogenerate: { directory: 'introduction' },
+      },
+      {
+        label: 'Руны',
+        autogenerate: { directory: 'runes' },
+      },
+      {
+        label: 'Синтаксис шаблонов',
+        autogenerate: { directory: 'template-syntax' },
+      },
+      {
+        label: 'Обучающие статьи и ролики',
+        link: 'learning',
+      },
+    ],
+  }), svelte()],
+});
