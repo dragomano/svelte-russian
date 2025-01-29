30fcb58677aa19e4f02f55b648df80a485f18978
Bugo <cheetah@bk.ru>
2025-01-29 21:39:52 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -45,6 +45,7 @@ export default defineConfig({
       },
       {
         label: 'Синтаксис шаблонов',
+        collapsed: true,
         autogenerate: { directory: 'template-syntax' },
       },
       {
