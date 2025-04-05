129e5ddbfe35a9ea49195ff13f633a43c09506dd
Bugo <cheetah@bk.ru>
2025-04-05 18:58:08 +0500

    Update content.config.ts

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,12 +1,13 @@
 import { defineCollection, z } from 'astro:content';
 import { docsLoader, i18nLoader } from "@astrojs/starlight/loaders";
 import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+import { topicSchema } from 'starlight-sidebar-topics/schema';
 
 export const collections = {
 	docs: defineCollection({
 		loader: docsLoader(),
 		schema: docsSchema({
-			extend: z.object({
+			extend: topicSchema.extend({
 				origin: z.string().url().optional(),
 			}),
 		})
