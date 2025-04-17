2cb9285f8568883fdbe7197d2bb3574b654a64c2
Bugo <cheetah@bk.ru>
2025-04-17 18:48:37 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -103,6 +103,14 @@ export default defineConfig({
           content: 'fq1A8llkn1XRVaPfBInN-TPoDtGsEUQmvEr3QVrEsyI',
         },
       },
+      {
+        tag: 'script',
+        content: `(function(c,l,a,r,i,t,y){
+          c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+          t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+          y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        })(window, document, "clarity", "script", "r5l1g73n2s");`,
+      },
     ],
     defaultLocale: 'root',
     locales: {
